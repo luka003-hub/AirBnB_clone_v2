f60a9844c8b510dcb8da6976c1344ac230addfc5
luka003-hub <kimathiluka03@gmail.com>
2024-03-09 06:02:01 -0800

    task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,9 +17,11 @@ def do_clean(number=0):
 
     try:
         with lcd("versions"):
-            local("ls -1t | tail -n +{} | xargs -I {{}} rm {{}}".format(number + 1))
+            local("ls -1t | tail -n +{} | xargs -I {{}} rm {{}}"
+                  .format(number + 1))
 
         with cd("/data/web_static/releases"):
-            run("ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1))
+            run("ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}"
+                .format(number + 1))
     except Exception:
         pass

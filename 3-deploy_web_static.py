239211203548400a5ff0a516e45c420b540759ab
luka003-hub <kimathiluka03@gmail.com>
2024-03-09 05:49:50 -0800

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to the web servers using the function deploy
+Fabric script that creates and distributes an archive
+to the web servers using the function deploy
 """
 
 from fabric.api import env, local, put, run
@@ -18,7 +19,8 @@ def do_pack():
         file_path = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_path))
         return file_path
-    except:
+    except Exception as e:
+        print(e)
         return None
 
 
@@ -28,18 +30,19 @@ def do_deploy(archive_path):
         return False
     try:
         archive_name = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        no_ext = archive_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('rm /tmp/{}'.format(archive_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 

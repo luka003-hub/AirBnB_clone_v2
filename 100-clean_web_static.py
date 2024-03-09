2e2a5e2f29e96fad68ea15690d392ad35e10dfe7
luka003-hub <kimathiluka03@gmail.com>
2024-03-09 05:39:35 -0800

    update

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Fabric script that deletes out-of-date archives using do_clean
+"""
+
+from fabric.api import local, run, lcd, env
+from datetime import datetime
+
+env.hosts = ['54.158.216.163', '3.84.168.12']
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+    number = int(number)
+    if number < 0:
+        return
+
+    try:
+        with lcd("versions"):
+            local("ls -1t | tail -n +{} | xargs -I {{}} rm {{}}".format(number + 1))
+
+        with cd("/data/web_static/releases"):
+            run("ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1))
+    except Exception:
+        pass

772bd3bcf82ee8e6efa156899a26aa5aa6f1f5ec
luka003-hub <kimathiluka03@gmail.com>
2024-04-18 22:07:28 +0000

    flask2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-
+"""
+A simple Flask web application
+"""
 
 from flask import Flask
-app = Flask(app)
 
+# Create the Flask application instance named 'app'
+app = Flask(__name__)
 
+# Define the route and corresponding function
 @app.route('/airbnb-onepage/')
 def hello():
     return "Hello, Airbnb One Page!"
 
-
-if app == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+# Run the Flask application if executed directly
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
